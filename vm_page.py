d358f3a14203d5e4b0991554cb3a3c67e2d03d5f
vikusido <vasidorkina@edu.hse.ru>
2025-03-10 16:57:57 +0300

    добавила немного текста про VM, еще допишу

--- vm_page.py
+++ vm_page.py
@@ -2,23 +2,29 @@ import streamlit as st
 
 
 def calculate_price(cpu, ram, storage, duration):
-    base_price = 10
-    cpu_cost = cpu * 2
-    ram_cost = ram * 1.5
-    storage_cost = storage * 0.1
-    duration_cost = duration * 0.5
+    base_price = 1000
+    cpu_cost = cpu * 20
+    ram_cost = ram * 15
+    storage_cost = storage * 10
+    duration_cost = duration * 50
     return base_price + cpu_cost + ram_cost + storage_cost + duration_cost
 
 def vm_page():
     st.title("Конфигуратор аренды виртуальных машин")
+    with st.container():
+        st.header("Что такое виртуальная машина?")
+        st.write("Виртуальная машина (VM) – это программная копия компьютера, работающая внутри другого. Она не существует как физическое устройство, но функционирует как полноценный компьютер, используя ресурсы основного. Представьте, что вы запускаете один компьютер внутри другого, создавая “воображаемую” машину, которая ведет себя так, как если бы была реальной.")
+        st.subheader("Как начать работу?")
+        st.write("Вы получаете ssh ключ к доступу VM")
+        st.subheader("Преимущества виртуальной машины")
+        
+        
     st.header("Параметры")
     cpu = st.slider("CPU Cores", min_value=1, max_value=32, value=4)
     ram = st.slider("RAM (GB)", min_value=1, max_value=128, value=8)
     storage = st.number_input("Storage (GB)", min_value=10, max_value=1000, step=10)
-    os_options = ["Ubuntu", "CentOS", "Windows Server"]
-    os = st.selectbox("Операционная система", options=os_options)
-    location_options = ["US East", "US West", "Europe", "Asia"]
-    location = st.selectbox("Расположение", options=location_options)
+    distribute_options = ["Ubuntu", "CentOS", "Fedora"]
+    distribute = st.selectbox("Дистрибутив Linux", options=distribute_options)
     duration = st.slider("Длительность аренды (минуты)", min_value=1, max_value=60, value=10)
 
     with st.container():
@@ -26,8 +32,7 @@ def vm_page():
         st.write(f"**CPU Cores:** {cpu}")
         st.write(f"**RAM:** {ram} GB")
         st.write(f"**Storage:** {storage} GB")
-        st.write(f"**Операционная система:** {os}")
-        st.write(f"**Расположение:** {location}")
+        st.write(f"**Дистрибутив Linux:** {distribute}")
         st.write(f"**Длительность аренды:** {duration} минут")
 
     price = calculate_price(cpu, ram, storage, duration)
