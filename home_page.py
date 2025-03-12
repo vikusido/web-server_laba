633e66573422c0ce67539c14a6e4f3ef09982552
vikusido <145844166+vikusido@users.noreply.github.com>
2025-03-12 18:14:20 +0300

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -3,4 +3,12 @@ import streamlit as st
 def home_page():
     st.header("Главная страница")
     st.write("Добро пожаловать!")
-    st.write("Здесь вы можете найти информацию о наших облачных решениях.")
+    st.write("Здесь вы можете арендовать виртуальную машину или контейнер.")
+    st.subheader("Как арендовать?")
+    st.write("Вы выбираете необходимые параметры (CPU, Storage, RAM и т.д.) и получаете SSH ключ для доступа.")
+    if st.button("Перейти к аренде виртуальных машин"):
+        st.session_state.page = "vm"
+        st.rerun()
+    if st.button("Перейти к аренде контейнеров"):
+        st.session_state.page = "container"
+        st.rerun()
